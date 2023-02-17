b2007f1e558ad7772cf7914f3dc173a56fcb7206
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-17 08:23:06 +0000

    4.2

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
-
+import styles from 'styles/header.module.css'
 export default function Header () {
   return (
     <header>
+      <div className={styles.fexContainer}>
         <Logo boxOn />
         <Nav />
+      </div>
     </header>
   )
 }
