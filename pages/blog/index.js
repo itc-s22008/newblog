e91405c4cbe783c4f1de1583ad10cec945e40919
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-17 08:52:25 +0000

    4.5

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,13 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Blog () {
   return (
+    <Container>
     <Hero
       title='blog'
       subtitle='recent Posts'
     />
+    </Container>
   )
 }
