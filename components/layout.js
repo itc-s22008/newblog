a06bedf0e2d3a3d6b3d2bfe2f2f15cb2e721d044
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-17 08:13:04 +0000

    4.2

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,11 @@
 import Header from 'components/header'
 import Footer from 'components/footer'
-
 export default function layout ({ children }) {
   return (
     <>
       <Header />
 
       <main>{children}</main>
-
       <Footer />
     </>
   )
