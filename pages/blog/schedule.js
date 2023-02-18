a260a5c45cf3d8aa76372ece17699c5f92c83a09
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-18 09:05:54 +0000

    8.1

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,6 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
-
+import PostHeader from 'components/pust-header'
 export default function Post({
   title,
   publish,
@@ -13,6 +13,9 @@ export default function Post({
 }) {
   return (
     <Container>
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish ={publish} />
+      </article>
       <h1>{title}</h1>
     </Container>
   )
