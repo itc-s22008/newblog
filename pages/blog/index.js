a1c4117e1c77755a186bf3e6a34b4aff38c0cb3b
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-19 07:53:38 +0000

    9.5

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,8 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
 export default function Blog () {
   return (
@@ -10,6 +12,17 @@ export default function Blog () {
       title='blog'
       subtitle='recent Posts'
     />
+    <Posts posts={posts} />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
